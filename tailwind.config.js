b21e939ad73b0928ead506261dcab1596fb8b68a
tannu kannaojiya <113445718+tannukannaojiya@users.noreply.github.com>
2024-04-09 15:02:40 +0530

    new

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      screen:{
+        'media873':'873',
+        'media1200':'1200px'
+        
+      },
+    },
+  },
+  plugins: [],
+}
+
